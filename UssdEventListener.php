f46a6e160ef6639701470af6eb8a586ba047bd7c
David Bwire <israelbwire@gmail.com>
2017-08-15 23:22:25 +0300

    add more guidance on how to work with ussd listeners

--- UssdEventListener.php
+++ UssdEventListener.php
@@ -26,15 +26,17 @@ class UssdEventListener extends InstantUssdEventListener {
                 // clear tracked menus
                 $isValidResponse = $e->getParam('is_valid', true);
                 if ($isValidResponse) {
+                    // this method should only be called by home menus
                     $this->clearMenuVisitHistory($e);
                 }
                 return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig, true, false);
             }
             // we have data sent in
-            // 1. Get latest response & save it; as it should be valid
+            // 1. Get latest response; it should be valid
             $latestResponse = $e->getLatestResponse();
             $lastServedMenu = $e->getName();
-            // 2. Determine next menu using latest response
+            // 2. Do your processing; save to db; etc
+            // 3. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
             return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
         });
     }
