7da1d9f85a86ac6fca21920beaecbf631af21932
David Bwire <israelbwire@gmail.com>
2017-08-17 04:21:06 +0300

    add sample ussd_menu config

--- config/iussd.config.php
+++ config/iussd.config.php
@@ -9,9 +9,9 @@ return [
         "ussd_event_listener" => UssdEventListener::class,
         'db' => [
             'driver' => 'Mysqli', // Mysqli, Sqlsrv, Pdo_Sqlite, Pdo_Mysql, Pdo(= Other PDO Driver)
-            'database' => 'your_db',
-            'username' => 'mysql_user',
-            'password' => 'mysql_pass',
+            'database' => '',
+            'username' => '',
+            'password' => '',
             //'hostname' => "",
             //'port' => "",
             //'charset' => "",
@@ -23,20 +23,110 @@ return [
         'ussd_menus' => [
             "home_instant_ussd" => [
                 "title" => "Welcome to InstantUssd",
-                "is_skippable" => false,
+                "body" => "",
                 "footer" => "Questions? Call +254712688559",
                 "menu_items_ranking_type" => UssdMenuConfig::RANKING_TYPE_PREDETERMINED,
-                "valid_values" => range(1, 2),
-                "loopset_name" => "",
+                "valid_values" => range(1, 3),
                 // menu_items
                 "menu_items" => [
                     [
-                        "next_menu_id" => 'example_enter_full_name',
-                        "description" => "Register"
+                        "next_menu_id" => 'iussd.register.self',
+                        "description" => "Register Self"
                     ],
                     [
-                        "next_menu_id" => 'example_my_account',
-                        "description" => "My account"
+                        "next_menu_id" => 'iussd.register.client.count',
+                        "description" => "Register Clients"
+                    ],
+                    [
+                        "next_menu_id" => 'iussd.my_account',
+                        "description" => "My Account"
+                    ]
+                ]
+            ],
+            "iussd.register.self" => [
+                "title" => "Register Yourself",
+                "body" => "Enter your First Name and Last Name",
+                "footer" => "",
+                "menu_items_ranking_type" => UssdMenuConfig::RANKING_TYPE_NONE,
+                "valid_values" => [],
+                // menu_items
+                "menu_items" => [
+                    [
+                        "next_menu_id" => 'iussd.final',
+                        "description" => ""
+                    ]
+                ]
+            ],
+            "iussd.register.client.count" => [
+                "title" => "Register Clients",
+                "body" => "How many clients would you like to register?",
+                "footer" => "",
+                "menu_items_ranking_type" => UssdMenuConfig::RANKING_TYPE_NONE,
+                "valid_values" => [],
+                // target_loopset - indicates the set of menu this
+                "target_loopset" => "register_clients",
+                // menu_items
+                "menu_items" => [
+                    [
+                        "next_menu_id" => 'iussd.register.client',
+                        "description" => ""
+                    ]
+                ]
+            ], "iussd.register.client" => [
+                "title" => "Register Client",
+                "body" => "Enter client's First Name and Last Name",
+                "footer" => "",
+                "menu_items_ranking_type" => UssdMenuConfig::RANKING_TYPE_NONE,
+                "valid_values" => [],
+                // loopset_name - a name that's shared by all screens within a 
+                // looping set
+                "loopset_name" => "register_clients",
+                // is_loop_end - indicates that this is the last screen within a
+                // loopset. We may proceed to next or go back to the first
+                // screen in the set
+                "is_loop_end" => true,
+                // loop_start - the screen to begin looping from
+                // can be itself or a previous one
+                "loop_start" => "iussd.register.client",
+                // menu_items
+                "menu_items" => [
+                    [
+                        "next_menu_id" => 'iussd.final',
+                        "description" => ""
+                    ]
+                ]
+            ],
+            "iussd.my_account" => [
+                "title" => "My Account",
+                "body" => "",
+                "footer" => "",
+                "menu_items_ranking_type" => UssdMenuConfig::RANKING_TYPE_PREDETERMINED,
+                "valid_values" => range(1, 2),
+                // menu_items
+                "menu_items" => [
+                    [
+                        "next_menu_id" => 'iussd.final',
+                        "description" => "Registered Clients"
+                    ],
+                    [
+                        "next_menu_id" => 'iussd.final',
+                        "description" => "Commission Received"
+                    ]
+                ]
+            ],
+            "iussd.final" => [
+                "title" => "Final Step",
+                "body" => "Thank you for trying-out InstantUssd. You may "
+                . "delete or edit the configs & listeners to suit your use case.",
+                "footer" => "",
+                "menu_items_ranking_type" => UssdMenuConfig::RANKING_TYPE_NONE,
+                "valid_values" => [],
+                // menu_items
+                "menu_items" => [
+                    [
+                        // empty next_menu_id will force an automatic exit
+                        // if user tries to send back a value
+                        "next_menu_id" => ""
                     ]
                 ]
             ]
