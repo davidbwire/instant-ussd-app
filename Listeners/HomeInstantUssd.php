804047b251e144e4781ff492ed0c0ee94799c72f
David Bwire <israelbwire@gmail.com>
2017-08-20 01:34:29 +0300

    add HomeInstantUssd listener

---
+++ Listeners/HomeInstantUssd.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InstantUssd\Listeners;
+
+use InstantUssd\Listeners\UssdListener;
+
+/**
+ * Description of UssdListener
+ *
+ * @author David Bwire
+ */
+class HomeInstantUssd extends UssdListener {
+    
+}
