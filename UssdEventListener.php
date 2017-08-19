1291b574165c1a24458da6e7c62e727745bbe6b2
David Bwire <israelbwire@gmail.com>
2017-08-20 01:36:16 +0300

    cut down size of ussd listeners on the sharedussdeventmanager

--- UssdEventListener.php
+++ UssdEventListener.php
@@ -4,6 +4,7 @@ namespace InstantUssd;
 
 use Bitmarshals\InstantUssd\UssdEventListener as InstantUssdEventListener;
 use Bitmarshals\InstantUssd\UssdEvent;
+use InstantUssd\Listeners;
 
 /**
  * Description of UssdEventListener
@@ -17,149 +18,26 @@ class UssdEventListener extends InstantUssdEventListener {
         // HOME PAGE example
         // example - attaching an event
         $this->attach('Bitmarshals\InstantUssd', 'home_instant_ussd', function($e) use ($ussdMenusConfig) {
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig     = $ussdMenusConfig[$e->getName()];
-            // we have data sent in
-            // 2. Get latest response; it should be valid
-            $latestResponse = $e->getLatestResponse();
-            $lastServedMenu = $e->getName();
-            // 3. Check if we should skip this listener
-            if (!$e->containsIncomingData()) {
-                $e->attachDynamicErrors($menuConfig);
-                // clear tracked menus
-                $isValidResponse = $e->getParam('is_valid', true);
-                if ($isValidResponse) {
-                    // this method should only be called by home menus (menus beginning with home_*)
-                    $e->getInstantUssd()->clearMenuVisitHistory($e);
-                }
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig, true, false);
-            }
-            // 4. Do your processing; save to db; etc
-            // 5. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
-        });
+            $listener = new Listeners\HomeInstantUssd($e, $ussdMenusConfig);
+            return call_user_func_array([$listener, "onTrigger"]);
+        });        
+        // Have a default listener for quick set up
+        $defaultListener = function($e) use ($ussdMenusConfig) {
+            $continueUssdHops     = true;
+            $appendNavigationText = false;
+            $listener             = new Listeners\UssdListener($e, $ussdMenusConfig);
+            return call_user_func_array([$listener, "onTrigger"], [$continueUssdHops, $appendNavigationText]);
+        };
         // REGISTER - SELF
-        $this->attach("Bitmarshals\InstantUssd", 'iussd.register.self', function($e) use ($ussdMenusConfig) {
-
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig        = $ussdMenusConfig[$e->getName()];
-            // 1. $shouldStopLooping should be called before a call to determineNextMenu
-            // $shouldStopLooping = $e->getInstantUssd()->shouldStopLooping($menuConfig, $e);
-            // we have data sent in
-            // 2. Get latest response; it should be valid
-            $latestResponse    = $e->getLatestResponse();
-            $lastServedMenu    = $e->getName();
-
-            /* / 3. *             * *---------------- SKIPPABLE MENU EXAMPLE 
-              $isSkippableMenu = $e->getInstantUssd()
-              ->getSkippableUssdMenuMapper()
-              ->isSkippable(['col_1' => $col1Val, 'col_2' => $col2Val, 'col_n' => $colNVal], $tableToCheck);
-              if ($isSkippableMenu) {
-              // stop propagation so that it is not captured
-              $e->stopPropagation(true);
-              // return a pointer to the menu we should skip to
-              $ussdMenuItem = $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse, $shouldStopLooping);
-              return $ussdMenuItem;
-              }
-              /* -------------------------------------------- */
-            if (!$e->containsIncomingData()) {
-                /* show this menu */
-                $e->attachDynamicErrors($menuConfig);
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig);
-            }
-            // 4. Do your processing; save to db; etc
-            // 5. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
-        });
+        $this->attach("Bitmarshals\InstantUssd", 'iussd.register.self', $defaultListener);
         // REGISTER - CLIENT [Number of Clients]
-        $this->attach("Bitmarshals\InstantUssd", 'iussd.register.client.count', function($e) use ($ussdMenusConfig) {
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig     = $ussdMenusConfig[$e->getName()];
-            // we have data sent in
-            // 2. Get latest response; it should be valid
-            $latestResponse = $e->getLatestResponse();
-            $lastServedMenu = $e->getName();
-
-            if (!$e->containsIncomingData()) {
-                /* show this menu */
-                $e->attachDynamicErrors($menuConfig);
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig);
-            }
-            $e->getInstantUssd()->getUssdLoopMapper()
-                    ->initializeLoopingSession($menuConfig['target_loopset'], $e->getParam('session_id'), $latestResponse);
-            // 4. Do your processing; save to db; etc
-            // 5. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
-        });
+        $this->attach("Bitmarshals\InstantUssd", 'iussd.register.client.count', $defaultListener);
         // REGISTER - CLIENT
-        $this->attach("Bitmarshals\InstantUssd", 'iussd.register.client', function($e) use ($ussdMenusConfig) {
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig        = $ussdMenusConfig[$e->getName()];
-            // 1.
-            $shouldStopLooping = $e->getInstantUssd()->shouldStopLooping($menuConfig, $e);
-            // we have data sent in
-            // 2. Get latest response; it should be valid
-            $latestResponse    = $e->getLatestResponse();
-            $lastServedMenu    = $e->getName();
-
-            if (!$e->containsIncomingData()) {
-                /* show this menu */
-                $e->attachDynamicErrors($menuConfig);
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig);
-            }
-            // 4. Do your processing; save to db; etc
-            // 5. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse, $shouldStopLooping);
-        });
+        $this->attach("Bitmarshals\InstantUssd", 'iussd.register.client', $defaultListener);
         // MY ACCOUNT
-        $this->attach("Bitmarshals\InstantUssd", 'iussd.my_account', function($e) use ($ussdMenusConfig) {
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig     = $ussdMenusConfig[$e->getName()];
-            // we have data sent in
-            // 2. Get latest response; it should be valid
-            $latestResponse = $e->getLatestResponse();
-            $lastServedMenu = $e->getName();
-
-            if (!$e->containsIncomingData()) {
-                /* show this menu */
-                $e->attachDynamicErrors($menuConfig);
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig);
-            }
-            // 4. Do your processing; save to db; etc
-            // 5. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
-        });
-
+        $this->attach("Bitmarshals\InstantUssd", 'iussd.my_account', $defaultListener);
         // EXIT STEP
-        $this->attach("Bitmarshals\InstantUssd", 'iussd.final', function($e) use ($ussdMenusConfig) {
-            if (!$e instanceof UssdEvent) {
-                return false;
-            }
-            $menuConfig     = $ussdMenusConfig[$e->getName()];
-            // we have data sent in
-            // 2. Get latest response; it should be valid
-            $latestResponse = $e->getLatestResponse();
-            $lastServedMenu = $e->getName();
-
-            if (!$e->containsIncomingData()) {
-                /* show this menu */
-                $e->attachDynamicErrors($menuConfig);
-                return $this->ussdResponseGenerator->composeAndRenderUssdMenu($menuConfig, false);
-            }
-            // 4. Do your processing; save to db; etc
-            // 5. Determine next menu using $lastServedMenu, $menuConfig and $latestResponse
-            return $this->ussdResponseGenerator->determineNextMenu($lastServedMenu, $menuConfig, $latestResponse);
-        });
+        $this->attach("Bitmarshals\InstantUssd", 'iussd.final', $defaultListener);
     }
 
 }
