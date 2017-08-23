d8310faca59a277ce14f5f95744c965df80e881c
David Bwire <israelbwire@gmail.com>
2017-08-23 18:53:40 +0300

    show all options that can be extracted

--- Listeners/HomeInstantUssd.php
+++ Listeners/HomeInstantUssd.php
@@ -13,7 +13,32 @@ class HomeInstantUssd extends UssdEventListener {
 
     public function captureIncomingData() {
 
+        $ussdEvent = $this->ussdEvent;
+
+        // client's phone number
+        $phoneNumber = $ussdEvent->getParam('phone_number');
+        // session_id
+        $sessionId = $ussdEvent->getParam('session_id');
+        // service code
+        $serviceCode = $ussdEvent->getParam('service_code');
+        // incoming response
         $latestResponse = $this->latestResponse;
+        // menu sending in data
+        $lastServedMenu = $this->lastServedMenu;
+        // configuration of menu sending in data
+        $menuConfig = $this->menuConfig;
+        // entire ussd_menus config
+        $ussdMenusConfig = $this->ussdMenusConfig;
+        // all responses sent in this session
+        $allResponses = $ussdEvent->getNonExtraneousValues();
+        // very first response
+        $firstResponse = $ussdEvent->getFirstResponse();
+        // your controller instance
+        $yourController = $ussdEvent->getInitializer();
+        // service locator
+        $serviceLocator = $ussdEvent->getServiceLocator();
+        // InstantUssd instance
+        $instantUssd = $ussdEvent->getInstantUssd();
         // save to db; etc
         return;
     }
